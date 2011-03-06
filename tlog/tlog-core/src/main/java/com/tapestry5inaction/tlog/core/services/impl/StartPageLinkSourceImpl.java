d7b1a5ae22c0ab85f20cf2126f1dd178f06b9836
Igor Drobiazko <drobiazko@apache.org>
2011-03-06 17:57:00 +0100

    Used addParameterValue instead addParameter when creating links with parameters

--- tlog/tlog-core/src/main/java/com/tapestry5inaction/tlog/core/services/impl/StartPageLinkSourceImpl.java
+++ tlog/tlog-core/src/main/java/com/tapestry5inaction/tlog/core/services/impl/StartPageLinkSourceImpl.java
@@ -6,10 +6,8 @@ import com.tapestry5inaction.tlog.core.entities.Month;
 import com.tapestry5inaction.tlog.core.entities.Tag;
 import com.tapestry5inaction.tlog.core.services.StartPageLinkSource;
 import org.apache.tapestry5.Link;
-import org.apache.tapestry5.ValueEncoder;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.PageRenderLinkSource;
-import org.apache.tapestry5.services.ValueEncoderSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +15,6 @@ import java.util.List;
 public class StartPageLinkSourceImpl implements StartPageLinkSource {
     @Inject
     private PageRenderLinkSource pageRenderLinkSource;
-
-    @Inject
-    private ValueEncoderSource valueEncoderSource;
 
     public Link getLink(Month month) {
         return getLink(month, RequestParameters.MONTH);
@@ -30,17 +25,15 @@ public class StartPageLinkSourceImpl implements StartPageLinkSource {
     }
 
     public Link getLink(Object value, String parameterName) {
-        ValueEncoder encoder = valueEncoderSource.getValueEncoder(value.getClass());
-
         Link link = pageRenderLinkSource.createPageRenderLinkWithContext("index");
 
         List<String> parameters = new ArrayList<String>(link.getParameterNames());
 
-        for(String next: parameters){
+        for (String next : parameters) {
             link.removeParameter(next);
         }
 
-        link.addParameter(parameterName, encoder.toClient(value));
+        link.addParameterValue(parameterName, value);
 
         return link;
     }
