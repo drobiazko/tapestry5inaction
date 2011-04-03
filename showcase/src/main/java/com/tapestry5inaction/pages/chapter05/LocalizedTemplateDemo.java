ea72151f438c01f34f0876ff19b3fea1973fb4c1
Igor Drobiazko <drobiazko@apache.org>
2011-04-03 14:21:00 +0200

    Added more localization examples

--- showcase/src/main/java/com/tapestry5inaction/pages/chapter05/LocalizedTemplateDemo.java
+++ showcase/src/main/java/com/tapestry5inaction/pages/chapter05/LocalizedTemplateDemo.java
@@ -1,11 +1,4 @@
 package com.tapestry5inaction.pages.chapter05;
 
-/**
- * Created by IntelliJ IDEA.
- * User: igor
- * Date: 03.04.11
- * Time: 14:08
- * To change this template use File | Settings | File Templates.
- */
 public class LocalizedTemplateDemo {
 }
--- showcase/src/main/java/com/tapestry5inaction/pages/chapter05/MessagesDemo.java
+++ showcase/src/main/java/com/tapestry5inaction/pages/chapter05/MessagesDemo.java
@@ -1,11 +1,24 @@
 package com.tapestry5inaction.pages.chapter05;
 
-/**
- * Created by IntelliJ IDEA.
- * User: igor
- * Date: 03.04.11
- * Time: 13:33
- * To change this template use File | Settings | File Templates.
- */
+
+import com.tapestry5inaction.entities.User;
+import org.apache.tapestry5.annotations.SessionState;
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
 public class MessagesDemo {
+
+    @Inject
+    private Messages messages;
+
+    @SessionState(create = false)
+    private User user;
+
+    public String getWelcomeMessage() {
+        if (user == null) {
+            return messages.get("welcome-guest");
+        }
+        return messages.format("welcome-user", user.getName());
+    }
+
 }
