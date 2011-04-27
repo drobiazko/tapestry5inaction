1613a17e8d8be7554a7e76f825f40e8f3f07ffaa
Igor Drobiazko <drobiazko@apache.org>
2011-04-27 22:37:49 +0200

    Added some examples on input validation

--- showcase/src/main/java/com/tapestry5inaction/pages/chapter06/DateFieldDemo.java
+++ showcase/src/main/java/com/tapestry5inaction/pages/chapter06/DateFieldDemo.java
@@ -1,8 +1,15 @@
 package com.tapestry5inaction.pages.chapter06;
 
 
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.ValidationException;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.corelib.components.DateField;
+import org.apache.tapestry5.corelib.components.Form;
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 import java.util.Date;
 
@@ -12,11 +19,30 @@ public class DateFieldDemo {
     @Property
     private Date birthday;
 
+    @InjectComponent
+    private Form form;
+
+    @InjectComponent
+    private DateField birthdayField;
+
+    @Inject
+    private Messages messages;
+
     @Property
-    private String message;
+    @Persist(PersistenceConstants.FLASH)
+    private String successMessage;
+
+    void onValidateFromBirthdayField(Date value) {
+        if (value != null && value.after(new Date())) {
+            form.recordError(birthdayField, messages.get("birthday-is-in-future"));
+        }
+    }
+
 
     void onSuccess() {
         //Process birthday here
-   }
+
+        successMessage = messages.format("success-message", birthday);
+    }
 
 }
