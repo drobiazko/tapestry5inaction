015a3d2d61027d12d83f13f45fad55bd864dbfd0
Igor Drobiazko <drobiazko@apache.org>
2011-12-15 22:50:14 +0100

    Examples for chapter 9

--- showcase/src/main/java/com/tapestry5inaction/components/SubComponent.java
+++ showcase/src/main/java/com/tapestry5inaction/components/SubComponent.java
@@ -2,5 +2,5 @@ package com.tapestry5inaction.components;
 
 import com.tapestry5inaction.base.BaseComponent;
 
-public class InheritedComponent extends BaseComponent{
+public class SubComponent extends BaseComponent{
 }
