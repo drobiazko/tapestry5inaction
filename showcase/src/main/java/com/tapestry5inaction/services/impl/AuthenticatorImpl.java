a67cae255ce1548799f16bc4958aed3933eb5261
Igor Drobiazko <idrobiazko@Igor-Drobiazkos-MacBook-Pro-2.local>
2012-03-19 14:49:47 +0100

    Refactored AuthenticatorImpl to use UserDao

--- showcase/src/main/java/com/tapestry5inaction/services/impl/AuthenticatorImpl.java
+++ showcase/src/main/java/com/tapestry5inaction/services/impl/AuthenticatorImpl.java
@@ -2,18 +2,28 @@ package com.tapestry5inaction.services.impl;
 
 import com.tapestry5inaction.entities.User;
 import com.tapestry5inaction.services.Authenticator;
+import com.tapestry5inaction.services.UserDao;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.ApplicationStateManager;
 
 public class AuthenticatorImpl implements Authenticator {
 
     @Inject
+    private UserDao userDao;
+
+    @Inject
     private ApplicationStateManager applicationStateManager;
 
     public User authenticate(String userName, String password) {
-        if ("admin".equals(userName) && "admin".equals(password)) {
-            return new User(userName, password);
+        User user = userDao.findByName(userName);
 
+        if (user != null) {
+            String digest = DigestUtils.md5Hex(password);
+
+            if (digest.equals(user.getPassword())) {
+                return user;
+            }
         }
 
         return null;
