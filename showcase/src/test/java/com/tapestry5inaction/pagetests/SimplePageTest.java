122ced62ae130b4c4ac9dcd7d596be87924401bc
Igor Drobiazko <idrobiazko@Igor-Drobiazkos-MacBook-Pro-2.local>
2012-03-19 15:06:17 +0100

    Added a simple clickLink test

--- showcase/src/test/java/com/tapestry5inaction/pagetests/SimplePageTest.java
+++ showcase/src/test/java/com/tapestry5inaction/pagetests/SimplePageTest.java
@@ -1,11 +1,14 @@
 package com.tapestry5inaction.pagetests;
 
 import org.apache.tapestry5.dom.Document;
+import org.apache.tapestry5.dom.Element;
+import org.apache.tapestry5.func.Predicate;
 import org.apache.tapestry5.test.PageTester;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 
@@ -29,5 +32,19 @@ public class SimplePageTest {
         Document document = pageTester.renderPage("index");
 
         assertTrue(document.toString().contains("Tapestry 5 Book"));
+
+        Element link = document.getRootElement().getElement(new Predicate<Element>() {
+            public boolean accept(Element element) {
+                String href = element.getAttribute("href");
+
+                return href != null && href.endsWith("/chapter04");
+            }
+        });
+
+        assertNotNull(link);
+
+        document = pageTester.clickLink(link);
+
+        assertTrue(document.toString().contains("This chapter covers:"));
     }
 }
